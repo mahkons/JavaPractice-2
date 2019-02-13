6b491be2d6eae0d7be18e15b44d682cb0d722493
Igor Smirnov <igor99_99@inbox.ru>
2019-02-13 18:20:25 +0300

    aaa

--- Streams/src/main/java/ru/hse/java/team/SecondPartTasks.java
+++ Streams/src/main/java/ru/hse/java/team/SecondPartTasks.java
@@ -1,7 +1,14 @@
 package ru.hse.java.team;
 
+import java.util.AbstractMap;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.random;
 
 public final class SecondPartTasks {
 
@@ -9,25 +16,45 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.stream()
+                .filter(s -> s.contains(sequence))
+                .collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
     // Стрелок атакует мишень и каждый раз попадает в произвольную точку квадрата.
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать, какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        final int N = 100000;
+        return (double) Stream.iterate(1, n -> n + 1)
+                .limit(N)
+                .map(x -> pow(random(), 2))
+                .map(x -> x + pow(random(), 2))
+                .filter(len -> len <= 1)
+                .count() / N;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        assert !compositions.isEmpty();
+
+        return compositions.entrySet()
+                .stream()
+                .map(e -> new AbstractMap.SimpleEntry<>(e.getKey(),
+                        String.join("", e.getValue())
+                                .length()))
+                .max(Comparator.comparing(AbstractMap.SimpleEntry::getValue))
+                .get()
+                .getKey();
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        List<Map.Entry<String, Integer>> result = orders.stream()
+                .flatMap(e -> e.entrySet().stream())
+                .collect(Collectors.toMap();
+
     }
 }
--- Streams/src/test/java/ru/hse/java/team/SecondPartTasksTest.java
+++ Streams/src/test/java/ru/hse/java/team/SecondPartTasksTest.java
@@ -2,23 +2,54 @@ package ru.hse.java.team;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
+import static java.lang.Math.PI;
+import static java.lang.StrictMath.abs;
+import static java.util.Map.entry;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static ru.hse.java.team.SecondPartTasks.*;
 
 public class SecondPartTasksTest {
 
     @Test
     public void testFindQuotes() {
-        fail();
+        var array = new ArrayList<String>();
+        array.add("aba");
+        array.add("caba");
+        array.add("abbb");
+        array.add("abba");
+
+        var result1 = new ArrayList<String>();
+        result1.add("aba");
+        result1.add("caba");
+
+        assertEquals(result1, findQuotes(array, "aba"));
+        assertEquals(new ArrayList<String>(),findQuotes(array, "bbbb"));
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        assertTrue(abs(piDividedBy4() - PI / 4) <= 1e-2);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        var map = Map.ofEntries(
+                entry("a", Arrays.asList("sup1", "sup2", "sup3")),
+                entry("b", Arrays.asList("sup12", "sup2", "sup3")),
+                entry("c", Arrays.asList("sup1", "sup2", "s22"))
+        );
+        assertEquals("b", findPrinter(map));
+
+        map = Map.ofEntries(
+                entry("a", Arrays.asList("sup1", "sup2", "sup3")),
+                entry("b", Arrays.asList("sup12", "sup2", "sup3")),
+                entry("c", Arrays.asList("sup10000", "sup2", "s22"))
+        );
+        assertEquals("c", findPrinter(map));
     }
 
     @Test
