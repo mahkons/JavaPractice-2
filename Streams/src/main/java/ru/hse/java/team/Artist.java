305eaf0d0dd4a6916127c9e8f711a0392e8a8a2c
Makhnev Konstantin <mah.kons@gmail.com>
2019-02-13 16:58:42 +0300

    Adding given files

---
+++ Streams/src/main/java/ru/hse/java/team/Artist.java
@@ -0,0 +1,14 @@
+package ru.hse.java.team;
+
+public class Artist {
+
+    private final String name;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Streams/src/test/java/ru/hse/java/team/SecondPartTasksTest.java
@@ -0,0 +1,28 @@
+package ru.hse.java.team;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class SecondPartTasksTest {
+
+    @Test
+    public void testFindQuotes() {
+        fail();
+    }
+
+    @Test
+    public void testPiDividedBy4() {
+        fail();
+    }
+
+    @Test
+    public void testFindPrinter() {
+        fail();
+    }
+
+    @Test
+    public void testCalculateGlobalOrder() {
+        fail();
+    }
+}
