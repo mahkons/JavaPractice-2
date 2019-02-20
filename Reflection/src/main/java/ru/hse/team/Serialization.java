0d5332818aaef871f4f5bde84ce9e713f6642f26
Makhnev Konstantin <mah.kons@gmail.com>
2019-02-20 16:14:54 +0300

    Simplest implementation

--- Reflection/src/main/java/ru/hse/team/Serialization.java
+++ Reflection/src/main/java/ru/hse/team/Serialization.java
@@ -2,9 +2,9 @@ package ru.hse.team;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,12 +21,67 @@ public class Serialization {
         return fields;
     }
 
-    public static void serialize(@NotNull Object object, @NotNull OutputStream outputStream) {
+    public static void serialize(@NotNull Object object, @NotNull OutputStream outputStream) throws IllegalAccessException, IOException {
 
+        var dataStream = new DataOutputStream(outputStream);
+        List<Field> allFields = getAllFields(object.getClass());
 
+        for (Field field : allFields) {
+            field.setAccessible(true);
+            if (field.getType() == boolean.class) {
+                dataStream.writeBoolean(field.getBoolean(object));
+            } else if (field.getType() == byte.class) {
+                dataStream.writeByte(field.getByte(object));
+            } else if (field.getType() == char.class) {
+                dataStream.writeChar(field.getChar(object));
+            } else if (field.getType() == double.class) {
+                dataStream.writeDouble(field.getDouble(object));
+            } else if (field.getType() == float.class) {
+                dataStream.writeFloat(field.getFloat(object));
+            } else if (field.getType() == int.class) {
+                dataStream.writeInt(field.getInt(object));
+            } else if (field.getType() == long.class) {
+                dataStream.writeLong(field.getLong(object));
+            } else if (field.getType() == short.class) {
+                dataStream.writeShort(field.getShort(object));
+            } else if (field.getType() == String.class) {
+                dataStream.writeUTF((String)field.get(object));
+            } else {
+                throw new IllegalArgumentException("object have a field which is both non-primitive and not a string");
+            }
+        }
     }
 
-    public static <T> T deserialize(@NotNull InputStream inputStream, @NotNull Class<T> clazz) {
+    public static <T> T deserialize(@NotNull InputStream inputStream, @NotNull Class<T> clazz)
+            throws IllegalAccessException, IOException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+        var dataStream = new DataInputStream(inputStream);
+        List<Field> allFields = getAllFields(clazz);
+        T object = clazz.getDeclaredConstructor().newInstance();
 
+        for (Field field : allFields) {
+            field.setAccessible(true);
+            if (field.getType() == boolean.class) {
+                field.setBoolean(object, dataStream.readBoolean());
+            } else if (field.getType() == byte.class) {
+                field.setByte(object, dataStream.readByte());
+            } else if (field.getType() == char.class) {
+                field.setChar(object, dataStream.readChar());
+            } else if (field.getType() == double.class) {
+                field.setDouble(object, dataStream.readDouble());
+            } else if (field.getType() == float.class) {
+                field.setFloat(object, dataStream.readFloat());
+            } else if (field.getType() == int.class) {
+                field.setInt(object, dataStream.readInt());
+            } else if (field.getType() == long.class) {
+                field.setLong(object, dataStream.readLong());
+            } else if (field.getType() == short.class) {
+                field.setShort(object, dataStream.readShort());
+            } else if (field.getType() == String.class) {
+                field.set(object, dataStream.readUTF());
+            } else {
+                throw new IllegalArgumentException("object have a field which is both non-primitive and not a string");
+            }
+        }
+        return object;
     }
 }
--- Reflection/src/test/java/ru/hse/team/SerializationTest.java
+++ Reflection/src/test/java/ru/hse/team/SerializationTest.java
@@ -1,7 +1,159 @@
 package ru.hse.team;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SerializationTest {
+
+    private class BoolAndIntValue {
+        boolean flag = true;
+        int value = 179;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof BoolAndIntValue)) return false;
+            BoolAndIntValue that = (BoolAndIntValue) o;
+            return flag == that.flag &&
+                    value == that.value;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(flag, value);
+        }
+    }
+
+    private class PrivateLongAdded {
+        private long some = 579;
+
+        public long getSome() {
+            return some;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof PrivateLongAdded)) return false;
+            PrivateLongAdded that = (PrivateLongAdded) o;
+            return getSome() == that.getSome();
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getSome());
+        }
+    }
+
+    private class LabeledValue extends BoolAndIntValue {
+        protected String best = "The Great String!";
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof LabeledValue)) return false;
+            if (!super.equals(o)) return false;
+            LabeledValue that = (LabeledValue) o;
+            return best.equals(that.best);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(super.hashCode(), best);
+        }
+    }
+
+    private class BigClass {
+        public boolean a = false;
+        public byte b = 10;
+        public char c = 'z';
+        protected double d = 1.0;
+        protected float e = -2.0f;
+        protected int f = Integer.MIN_VALUE;
+        private long g = Long.MAX_VALUE;
+        private short h = 0;
+        private String i = "Label\nHA!";
+
+        public long getG() {
+            return g;
+        }
+
+        public short getH() {
+            return h;
+        }
+
+        public String getI() {
+            return i;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof BigClass)) return false;
+            BigClass bigClass = (BigClass) o;
+            return a == bigClass.a &&
+                    b == bigClass.b &&
+                    c == bigClass.c &&
+                    Double.compare(bigClass.d, d) == 0 &&
+                    Float.compare(bigClass.e, e) == 0 &&
+                    f == bigClass.f &&
+                    getG() == bigClass.getG() &&
+                    getH() == bigClass.getH() &&
+                    getI().equals(bigClass.getI());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(a, b, c, d, e, f, getG(), getH(), getI());
+        }
+    }
+
+    private BoolAndIntValue boolAndIntValue = new BoolAndIntValue();
+    private PrivateLongAdded privateLongAdded = new PrivateLongAdded();
+    private LabeledValue labeledValue = new LabeledValue();
+    private BigClass bigClass = new BigClass();
+
+    @Test
+    void serializeAndDeserialize_simple()
+            throws IllegalAccessException, IOException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
+        Serialization.serialize(boolAndIntValue, byteArrayOutputStream);
+        assertEquals(boolAndIntValue, Serialization.deserialize(
+                new ByteArrayInputStream(byteArrayOutputStream.toByteArray()), BoolAndIntValue.class));
+    }
+
+    @Test
+    void serializeAndDeserialize_withPrivate()
+            throws IllegalAccessException, IOException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
+        Serialization.serialize(privateLongAdded, byteArrayOutputStream);
+        assertEquals(privateLongAdded, Serialization.deserialize(
+                new ByteArrayInputStream(byteArrayOutputStream.toByteArray()), PrivateLongAdded.class));
+    }
+
+    @Test
+    void serializeAndDeserialize_inheritance()
+            throws IllegalAccessException, IOException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
+        Serialization.serialize(labeledValue, byteArrayOutputStream);
+        assertEquals(labeledValue, Serialization.deserialize(
+                new ByteArrayInputStream(byteArrayOutputStream.toByteArray()), LabeledValue.class));
+    }
+
+    @Test
+    void serializeAndDeserialize_allProbableFieldTypes()
+            throws IllegalAccessException, IOException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
+        Serialization.serialize(bigClass, byteArrayOutputStream);
+        assertEquals(bigClass, Serialization.deserialize(
+                new ByteArrayInputStream(byteArrayOutputStream.toByteArray()), BigClass.class));
+    }
 
 }
