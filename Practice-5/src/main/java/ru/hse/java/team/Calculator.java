def913be8d5b3429cb4c2d4c7edb8f4810e25b5b
Makhnev Konstantin <mah.kons@gmail.com>
2019-02-27 17:20:31 +0300

    Calculator for integers

--- Practice-5/src/main/java/ru/hse/java/team/Calculator.java
+++ Practice-5/src/main/java/ru/hse/java/team/Calculator.java
@@ -5,32 +5,33 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Scanner;
 import java.util.Stack;
 import java.util.function.DoubleBinaryOperator;
+import java.util.function.IntBinaryOperator;
 
 public class Calculator {
 
-    private final Stack<Double> stack;
+    private final Stack<Integer> stack;
 
-    public Calculator(@NotNull Stack<Double> stack) {
+    public Calculator(@NotNull Stack<Integer> stack) {
         this.stack = stack;
     }
 
-    private enum Operation implements DoubleBinaryOperator {
+    private enum Operation implements IntBinaryOperator {
         PLUS    ("+", (l, r) -> l + r),
         MINUS   ("-", (l, r) -> l - r),
         MULTIPLY("*", (l, r) -> l * r),
         DIVIDE  ("/", (l, r) -> l / r);
 
         private final String symbol;
-        private final DoubleBinaryOperator binaryOperator;
+        private final IntBinaryOperator binaryOperator;
 
-        private Operation(final String symbol, final DoubleBinaryOperator binaryOperator) {
+        private Operation(final String symbol, final IntBinaryOperator binaryOperator) {
             this.symbol = symbol;
             this.binaryOperator = binaryOperator;
         }
 
         public static Operation fromString(final String symbol) {
             for (Operation operation : Operation.values()) {
-                if (operation.toString().equals(symbol)) {
+                if (operation.symbol.equals(symbol)) {
                     return operation;
                 }
             }
@@ -42,8 +43,8 @@ public class Calculator {
         }
 
         @Override
-        public double applyAsDouble(final double left, final double right) {
-            return binaryOperator.applyAsDouble(left, right);
+        public int applyAsInt(final int left, final int right) {
+            return binaryOperator.applyAsInt(left, right);
         }
     }
 
@@ -53,8 +54,8 @@ public class Calculator {
         scanner.useDelimiter(" ");
 
         while (scanner.hasNext()) {
-            if (scanner.hasNextDouble()) {
-                Double number = scanner.nextDouble();
+            if (scanner.hasNextInt()) {
+                Integer number = scanner.nextInt();
                 stack.push(number);
             } else {
                 String token = scanner.next();
@@ -67,9 +68,9 @@ public class Calculator {
                     stack.clear();
                     throw new IllegalArgumentException("String parse error: not enough numbers in stack to perform operation");
                 }
-                Double firstNumber = stack.pop();
-                Double secondNumber = stack.pop();
-                stack.push(operation.applyAsDouble(firstNumber, secondNumber));
+                Integer firstNumber = stack.pop();
+                Integer secondNumber = stack.pop();
+                stack.push(operation.applyAsInt(firstNumber, secondNumber));
             }
         }
         if (stack.empty()) {
