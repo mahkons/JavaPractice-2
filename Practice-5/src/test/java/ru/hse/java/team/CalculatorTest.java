b736deeb87927c2d7b88aeadd31d6ab480a62f37
Igor Smirnov <igor99_99@inbox.ru>
2019-02-27 17:37:48 +0300

    updated tests

--- Practice-5/src/test/java/ru/hse/java/team/CalculatorTest.java
+++ Practice-5/src/test/java/ru/hse/java/team/CalculatorTest.java
@@ -8,34 +8,37 @@ import java.util.Stack;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@SuppressWarnings("unchecked")
 class CalculatorTest {
 
     @Test
     void simplePlusOperation() {
         Stack<Integer> mockedStack = mock(Stack.class);
         when(mockedStack.pop()).thenReturn(3,2, 5);
+        when(mockedStack.size()).thenReturn(2, 1);
+
+        var calculator = new Calculator(mockedStack);
+        assertEquals(5, calculator.evaluate("2 3 +"));
 
         InOrder inOrder = inOrder(mockedStack);
         inOrder.verify(mockedStack).push(2);
         inOrder.verify(mockedStack).push(3);
         inOrder.verify(mockedStack).push(5);
-
-        var calculator = new Calculator(mockedStack);
-        assertEquals(5, calculator.evaluate("2 3 +"));
     }
 
     @Test
     void simpleMinusOperation() {
         Stack<Integer> mockedStack = mock(Stack.class);
         when(mockedStack.pop()).thenReturn(3, 2, -1);
+        when(mockedStack.size()).thenReturn(2, -1);
+
+        var calculator = new Calculator(mockedStack);
+        assertEquals(-1, calculator.evaluate("2 3 -"));
 
         InOrder inOrder = inOrder(mockedStack);
         inOrder.verify(mockedStack).push(2);
         inOrder.verify(mockedStack).push(3);
         inOrder.verify(mockedStack).push(-1);
-
-        var calculator = new Calculator(mockedStack);
-        assertEquals(-1, calculator.evaluate("2 3 -"));
     }
 
     @Test
@@ -43,17 +46,21 @@ class CalculatorTest {
         Stack<Integer> mockedStack = mock(Stack.class);
         when(mockedStack.pop()).thenReturn(-1);
 
-        InOrder inOrder = inOrder(mockedStack);
-        inOrder.verify(mockedStack).push(-1);
-
         var calculator = new Calculator(mockedStack);
         assertEquals(-1, calculator.evaluate("-1"));
+
+        InOrder inOrder = inOrder(mockedStack);
+        inOrder.verify(mockedStack).push(-1);
     }
 
     @Test
     void twoOperations() {
         Stack<Integer> mockedStack = mock(Stack.class);
         when(mockedStack.pop()).thenReturn(3, 2, 2, 5, 7);
+        when(mockedStack.size()).thenReturn(2, 2, 1);
+
+        var calculator = new Calculator(mockedStack);
+        assertEquals(7, calculator.evaluate("2 3 + 2 +"));
 
         InOrder inOrder = inOrder(mockedStack);
         inOrder.verify(mockedStack).push(2);
@@ -61,8 +68,22 @@ class CalculatorTest {
         inOrder.verify(mockedStack).push(5);
         inOrder.verify(mockedStack).push(2);
         inOrder.verify(mockedStack).push(7);
+    }
+
+    @Test
+    void divisionAndMultiply() {
+        Stack<Integer> mockedStack = mock(Stack.class);
+        when(mockedStack.pop()).thenReturn(3, 2, 2, 0, 0);
+        when(mockedStack.size()).thenReturn(2, 2, 1);
 
         var calculator = new Calculator(mockedStack);
-        assertEquals(-1, calculator.evaluate("2 3 + 2 +"));
+        assertEquals(0, calculator.evaluate("2 3 / 2 *"));
+
+        InOrder inOrder = inOrder(mockedStack);
+        inOrder.verify(mockedStack).push(2);
+        inOrder.verify(mockedStack).push(3);
+        inOrder.verify(mockedStack).push(0);
+        inOrder.verify(mockedStack).push(2);
+        inOrder.verify(mockedStack).push(0);
     }
 }
