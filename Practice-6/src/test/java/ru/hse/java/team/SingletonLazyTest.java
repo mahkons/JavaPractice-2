a5d347e0e36024944a0e07029276b43c394271a0
Makhnev Konstantin <mah.kons@gmail.com>
2019-04-10 15:54:42 +0300

    tests started for singleton version

--- Practice-6/src/test/java/ru/hse/java/team/SingletonLazyTest.java
+++ Practice-6/src/test/java/ru/hse/java/team/SingletonLazyTest.java
@@ -1,7 +1,64 @@
 package ru.hse.java.team;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SingletonLazyTest {
+
+    public class NullSupplier implements Supplier<Object> {
+        int counter = 0;
+        @Override
+        public Object get() {
+            synchronized (this) {
+                counter++;
+                if (counter != 1) {
+                    throw new AssertionError("NullSupplier called more than once");
+                }
+            }
+            return null;
+        }
+    }
+
+    public class SimpleSupplier implements Supplier<Object> {
+
+        @Override
+        public Object get() {
+            return new Object();
+        }
+    }
+
+    public class SupplierWithCounter implements Supplier<Object> {
+
+        private AtomicInteger counter = new AtomicInteger();
+        @Override
+        public Object get() {
+            int value = counter.incrementAndGet();
+            if (value != 1) {
+                throw new AssertionError("SupplierWithCounter called more than once");
+            }
+            return new Object();
+        }
+    }
+
+
+    @Test
+    void nullSuppler() {
+        final Supplier<Object> supplier = new SimpleSupplier();
+        
+    }
+
+    @Test
+    void simpleSupplier() {
+
+    }
+
+    @Test
+    void supplerWithCounter() {
+
+    }
 
 }
