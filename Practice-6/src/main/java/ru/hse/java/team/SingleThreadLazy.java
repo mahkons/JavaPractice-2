c5a6ea235a874bf3993440b42a193115dfcd6efc
Gavrilov <gavrilov_dan@mail.ru>
2019-04-10 16:09:50 +0300

    SingleThreadLazy implemented

--- Practice-6/src/main/java/ru/hse/java/team/SingleThreadLazy.java
+++ Practice-6/src/main/java/ru/hse/java/team/SingleThreadLazy.java
@@ -1,4 +1,19 @@
 package ru.hse.java.team;
 
-public class SingleThreadLazy {
+public class SingleThreadLazy<T> implements Lazy<T> {
+    private T result;
+    private Supplier<T> supplier;
+
+    public SingleThreadLazy(Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+
+    public T get() {
+        if (supplier == null) {
+            return result;
+        }
+        result = supplier.get();
+        supplier = null;
+        return result;
+    }
 }
