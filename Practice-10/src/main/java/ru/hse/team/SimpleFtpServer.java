637efb5ad731e58ae7f2c542a35e67f6a77e465f
Makhnev Konstantin <mah.kons@gmail.com>
2019-05-22 16:59:34 +0300

    server

--- Practice-10/src/main/java/ru/hse/team/SimpleFtpServer.java
+++ Practice-10/src/main/java/ru/hse/team/SimpleFtpServer.java
@@ -1,4 +1,134 @@
 package ru.hse.team;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class SimpleFtpServer {
+
+    private static int serverPort = 9999;
+    private static int bufferSize = 256;
+    private final ServerSocketChannel serverSocketChannel;
+
+    private Selector selector = Selector.open();
+
+    private ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() - 1);
+    private volatile boolean serverRunning = true;
+
+    public SimpleFtpServer() throws IOException {
+        serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress(serverPort));
+        serverSocketChannel.configureBlocking(false);
+
+        int options = serverSocketChannel.validOps();
+        serverSocketChannel.register(selector, options, null);
+    }
+
+    private void handleRequest(ByteBuffer buffer, SocketChannel connection) {
+        String request = new String(buffer.array(), StandardCharsets.UTF_8);
+
+        try {
+
+            byte[] message;
+            if (request.charAt(0) == '1') {
+                message = list(request.substring(2, request.length()));
+            } else {
+                message = get(request.substring(2, request.length()));
+            }
+
+            buffer = ByteBuffer.wrap(message);
+            while (connection.write(buffer) != -1) { }
+        } catch (IOException exception) {
+            //AAA
+        }
+    }
+
+    private char isDirectoryChar(boolean flag) {
+        if (flag)
+            return '1';
+        else
+            return '0';
+    }
+
+    private byte[] list(String pathName) throws IOException {
+        String message;
+        Path path = Paths.get(pathName);
+        if (!Files.exists(path)) {
+            message = "-1";
+        } else {
+            message = Files.walk(path).map(x -> x + " " + isDirectoryChar(Files.isDirectory(path)) + " ").collect(Collectors.joining());
+        }
+        return message.getBytes(StandardCharsets.UTF_8);
+    }
+
+    private byte[] get(String pathName) {
+        String message = "";
+        Path path = Paths.get(pathName);
+
+        try {
+            if (!Files.exists(path)) {
+                message = "-1";
+            } else {
+                try (var input = new FileInputStream(pathName)) {
+                    byte[] bytes = input.readAllBytes();
+                    message = bytes.length + " " + new String(bytes, StandardCharsets.UTF_8);
+                }
+            }
+        } catch (IOException exception) {
+            //AA
+        }
+        return message.getBytes(StandardCharsets.UTF_8);
+    }
+
+    public void start() throws IOException {
+
+        while(serverRunning) {
+            selector.select();
+
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> keysIterator = selectedKeys.iterator();
+
+            while (keysIterator.hasNext()) {
+                SelectionKey key = keysIterator.next();
+
+                if (key.isAcceptable()) {
+                    SocketChannel clientChannel = serverSocketChannel.accept();
+                    clientChannel.configureBlocking(false);
+                    ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+                    clientChannel.register(selector, SelectionKey.OP_READ, buffer);
+                } else if (key.isReadable()) {
+
+                    SocketChannel connection = (SocketChannel) key.channel();
+                    if (connection.read((ByteBuffer)key.attachment()) == -1) {
+                        threadPool.submit(() -> handleRequest((ByteBuffer)key.attachment(), connection));
+                    }
+
+                }
+                keysIterator.remove();
+            }
+        }
+    }
+
+    public void stop() {
+        serverRunning = false;
+        threadPool.shutdown();
+    }
 }
