d7a8256f854b23dd05cf6a6e074866154ac9fcbb
Zuev Maxim <max-zu99@mail.ru>
2019-06-05 00:12:10 +0300

    client-server communication

--- Practice-10/src/main/java/ru/hse/team/SimpleFtpClient.java
+++ Practice-10/src/main/java/ru/hse/team/SimpleFtpClient.java
@@ -1,12 +1,10 @@
 package ru.hse.team;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 public class SimpleFtpClient {
     private SocketChannel socketChannel;
@@ -14,43 +12,55 @@ public class SimpleFtpClient {
     public void connect() throws IOException {
         InetSocketAddress address = new InetSocketAddress("localhost", 9999);
         socketChannel = SocketChannel.open(address);
-        socketChannel.configureBlocking(false);
     }
 
     public void disconnect() throws IOException {
         socketChannel.close();
     }
 
-    public String executeList(String path) throws IOException {
-        byte[] message = ("1 " + path).getBytes(StandardCharsets.UTF_8);
-        ByteBuffer buffer = ByteBuffer.wrap(message);
-        socketChannel.write(buffer);
+    public String execute(int id, String path) throws IOException {
+        var message = (id + " " + path).getBytes(StandardCharsets.UTF_8);
+        var length = message.length;
+        var sizeBuffer = ByteBuffer.allocate(4);
+        var buffer = ByteBuffer.allocate(4048);
+        var buffers = new ByteBuffer[] {sizeBuffer, buffer};
+
+        sizeBuffer.clear();
         buffer.clear();
-        socketChannel.read(buffer);
-        String response = new String(buffer.array(), StandardCharsets.UTF_8);
+        sizeBuffer.putInt(length);
+        buffer.put(message);
+        sizeBuffer.flip();
+        buffer.flip();
+        System.out.println("Writing data...");
+        socketChannel.write(buffers);
+        System.out.println("Data is written.");
+        sizeBuffer.clear();
+        buffer.clear();
+        System.out.println("Waiting for response...");
+        socketChannel.read(buffers);
+        System.out.println("Got response!");
+        sizeBuffer.flip();
+        buffer.flip();
+        length = sizeBuffer.getInt();
+        String response = new String(buffer.array(), 0, length, StandardCharsets.UTF_8);
         buffer.clear();
         return response;
+    }
+
+    public String executeList(String path) throws IOException {
+        return execute(1, path);
     }
 
     public String executeGet(String path) throws IOException {
-        byte[] message = ("2 " + path).getBytes(StandardCharsets.UTF_8);
-        ByteBuffer buffer = ByteBuffer.wrap(message);
-        socketChannel.write(buffer);
-        buffer.clear();
-        socketChannel.read(buffer);
-        String response = new String(buffer.array(), StandardCharsets.UTF_8);
-        buffer.clear();
-        return response;
-    }
-
-    void createFile(String name, byte[] content) {
-        //File file = Files.createFile(name);
+        return execute(2, path);
     }
 
     public static void main(String[] args) throws IOException {
         var client = new SimpleFtpClient();
         client.connect();
-        System.out.println(client.executeGet("docs/"));
+        // Только один запрос за раз!
+        System.out.println(client.executeList("./src/main/java/ru/hse/team"));
+        //System.out.println(client.executeGet("./src/main/java/ru/hse/team/SimpleFtpClient.java"));
         client.disconnect();
     }
 }
--- Practice-10/src/main/java/ru/hse/team/SimpleFtpServer.java
+++ Practice-10/src/main/java/ru/hse/team/SimpleFtpServer.java
@@ -24,12 +24,12 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class SimpleFtpServer {
-
+    private static final int INT_SIZE = 4;
     private static int serverPort = 9999;
-    private static int bufferSize = 256;
+    private static int bufferSize = 4048;
     private final ServerSocketChannel serverSocketChannel;
 
-    private Selector selector = Selector.open();
+    private final Selector selector = Selector.open();
 
     private ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() - 1);
     private boolean serverRunning = true;
@@ -38,108 +38,117 @@ public class SimpleFtpServer {
         serverSocketChannel = ServerSocketChannel.open();
         serverSocketChannel.socket().bind(new InetSocketAddress(serverPort));
         serverSocketChannel.configureBlocking(false);
-
-        int options = serverSocketChannel.validOps();
-        serverSocketChannel.register(selector, options, null);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
     }
 
-    private void handleRequest(ByteBuffer buffer, SocketChannel connection) {
-        try {
-            while (connection.read(buffer) != -1) {
-            }
-        } catch (IOException exception) {
-            //
-        }
-        buffer.flip();
+    public void start() {
+        new Thread(() -> {
+            try {
+                while (serverRunning) {
+                    System.out.println("Waiting for clients...");
+                    int n = selector.select(1000);
+                    if (n == 0) {
+                        continue;
+                    }
+                    System.out.println("Got a job!");
+                    var selectedKeys = selector.selectedKeys();
+                    var it = selectedKeys.iterator();
+                    while (it.hasNext()) {
+                        var key = it.next();
+                        if (key.isAcceptable()) {
+                            var server = (ServerSocketChannel) key.channel();
+                            var channel = server.accept();
+                            System.out.println("Got a client!");
+                            channel.configureBlocking(false);
+                            channel.register(selector, SelectionKey.OP_READ, new Client(channel));
+                        } else if (key.isReadable()) {
+                            var client = (Client) key.attachment();
+                            System.out.println("Reading from client " + client.toString());
+                            client.channel.read(client.buffers);
+                            if (client.size == -1 && client.sizeBuffer.position() == INT_SIZE) {
+                                client.sizeBuffer.flip();
+                                client.size = client.sizeBuffer.getInt();
+                            }
+                            if (client.size != -1 && client.buffer.position() == client.size) {
+                                key.cancel();
+                                System.out.println("Read from client " + client.toString() + " fully.");
+                                threadPool.submit(new ClientTask(client));
+                            }
+                        } else if (key.isWritable()) {
+                            var client = (Client) key.attachment();
+                            System.out.println("Writing client " + client.toString());
+                            client.channel.write(client.buffers);
+                            if (client.sizeBuffer.position() == INT_SIZE && client.buffer.position() == client.buffer.limit()) {
+                                System.out.println("Write client " + client.toString() + " fully.");
+                                client.buffer.clear();
+                                client.sizeBuffer.clear();
+                                client.size = -1;
+                                key.cancel();
+                            }
+                        }
+                        it.remove();
+                    }
 
-        String request = new String(buffer.array(), StandardCharsets.UTF_8).trim();
-
-        try {
-
-            byte[] message;
-            if (request.charAt(0) == '1') {
-                message = list(request.substring(2));
-            } else {
-                message = get(request.substring(2));
-            }
-
-            ByteBuffer someBuffer = ByteBuffer.wrap(message);
-            while (someBuffer.hasRemaining()) {
-                connection.write(someBuffer);
-            }
-            connection.close();
-        } catch (IOException exception) {
-            //AAA
-        }
-
-        buffer.clear();
-    }
-
-    private char isDirectoryChar(boolean flag) {
-        if (flag)
-            return '1';
-        else
-            return '0';
-    }
-
-    private byte[] list(String pathName) throws IOException {
-        String message;
-        Path path = Paths.get(pathName);
-        if (!Files.exists(path)) {
-            message = "-1";
-        } else {
-            List<String> list = Files.walk(path).map(x -> x + " " + isDirectoryChar(Files.isDirectory(path))).collect(Collectors.toList());
-            message = list.size() + " " + String.join(" ", list);
-        }
-        return message.getBytes(StandardCharsets.UTF_8);
-    }
-
-    private byte[] get(String pathName) {
-        String message = "";
-        Path path = Paths.get(pathName);
-
-        try {
-            if (!Files.exists(path)) {
-                message = "-1";
-            } else {
-                try (var input = new FileInputStream(pathName)) {
-                    byte[] bytes = input.readAllBytes();
-                    message = bytes.length + " " + new String(bytes, StandardCharsets.UTF_8);
-                }
-            }
-        } catch (IOException exception) {
-            //AA
-        }
-        return message.getBytes(StandardCharsets.UTF_8);
-    }
-
-    public void start() throws IOException {
-
-        while(serverRunning) {
-            selector.select();
-
-            Set<SelectionKey> selectedKeys = selector.selectedKeys();
-            Iterator<SelectionKey> keysIterator = selectedKeys.iterator();
-
-            while (keysIterator.hasNext()) {
-                SelectionKey key = keysIterator.next();
-
-                if (key.isAcceptable()) {
-                    SocketChannel clientChannel = serverSocketChannel.accept();
-                    clientChannel.configureBlocking(false);
-                    ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
-                    clientChannel.register(selector, SelectionKey.OP_READ, buffer);
-                } else if (key.isReadable()) {
-
-                    SocketChannel connection = (SocketChannel) key.channel();
-                    connection.register(selector, 0);
-                    threadPool.submit(() -> handleRequest((ByteBuffer)key.attachment(), connection));
 
                 }
-                keysIterator.remove();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+        }).start();
+    }
+
+    private class ClientTask implements Runnable {
+        private Client client;
+
+        private ClientTask(Client client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.println("Got work request from client " + client.toString());
+                client.buffer.flip();
+                var data = new byte[client.size];
+                client.buffer.get(data);
+                client.buffer.clear();
+                client.sizeBuffer.clear();
+                String request = new String(data, StandardCharsets.UTF_8);
+                byte[] respondData;
+                assert request.length() > 2 && (request.startsWith("1 ") || request.startsWith("2 "));
+                if (request.charAt(0) == '1') {
+                    respondData = list(request.substring(2));
+                } else {
+                    respondData = get(request.substring(2));
+                }
+                client.sizeBuffer.putInt(respondData.length);
+                client.buffer.put(respondData);
+                client.sizeBuffer.flip();
+                client.buffer.flip();
+                System.out.println("Work for client " + client.toString() + " done!");
+                client.channel.register(selector, SelectionKey.OP_WRITE, client);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
+
+    private class Client {
+        private int size = -1;
+        private final ByteBuffer sizeBuffer = ByteBuffer.allocate(INT_SIZE);
+        private final ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+        private final SocketChannel channel;
+        private final ByteBuffer[] buffers = new ByteBuffer[] {sizeBuffer, buffer};
+
+        private Client(SocketChannel channel) {
+            sizeBuffer.clear();
+            buffer.clear();
+            this.channel = channel;
+        }
+    }
+
+
 
     public static void main(String[] args) throws IOException {
         SimpleFtpServer server = new SimpleFtpServer();
@@ -151,5 +160,48 @@ public class SimpleFtpServer {
         threadPool.shutdown();
         serverSocketChannel.close();
         selector.close();
+    }
+
+
+    private static char isDirectoryChar(boolean flag) {
+        if (flag)
+            return '1';
+        else
+            return '0';
+    }
+
+    private static byte[] list(String pathName) throws IOException {
+        System.out.println("Answer list request... " + pathName);
+        String message;
+        Path path = Paths.get(pathName);
+        if (!Files.exists(path)) {
+            message = "-1";
+        } else {
+            List<String> list = Files.walk(path, 1).map(x -> x + " " + isDirectoryChar(x.toFile().isDirectory())).collect(Collectors.toList());
+            message = list.size() + " " + String.join(" ", list);
+        }
+        System.out.println("List answer is ready.");
+        return message.getBytes(StandardCharsets.UTF_8);
+    }
+
+    private static byte[] get(String pathName) {
+        System.out.println("Answer get request... " + pathName);
+        String message = "";
+        Path path = Paths.get(pathName);
+
+        try {
+            if (!Files.exists(path) || !Files.isRegularFile(path)) {
+                message = "-1";
+            } else {
+                try (var input = new FileInputStream(pathName)) {
+                    byte[] bytes = input.readAllBytes();
+                    message = bytes.length + " " + new String(bytes, StandardCharsets.UTF_8);
+                }
+            }
+        } catch (IOException exception) {
+            //AA
+        }
+        System.out.println("Get answer is ready.");
+        return message.getBytes(StandardCharsets.UTF_8);
     }
 }
